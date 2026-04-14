c37d66381924994f9aa7cb65e9de82cabc28e05a
chhaya <vermachhaya93@gmail.com>
2026-04-14 14:46:12 +0530

    first class

---
+++ phase1.java
@@ -0,0 +1,62 @@
+class Document {
+
+
+    private StringBuffer textBuffer;
+
+    public Document() {
+        textBuffer = new StringBuffer();
+    }
+
+    
+    public void addText(String input) {
+        textBuffer.append(input);
+    }
+
+    
+    public void clearText() {
+        textBuffer.setLength(0);
+    }
+
+
+    public void reverseText() {
+        textBuffer.reverse();
+    }
+
+    
+    public void deleteText(int start, int end) {
+       
+    if (start >= 0 && end <= textBuffer.length() && start < end) {
+        textBuffer.delete(start, end);
+    } else {
+        System.out.println("Invalid index range");
+    }
+}    
+
+    
+    public void displayText() {
+        System.out.println(textBuffer.toString());
+    }
+
+}
+
+public class phase1 {
+    public static void main(String[] args) {
+        Document doc = new Document();
+        doc.addText("yahajdoa");
+        System.out.print("After adding: ");
+        doc.displayText();
+        doc.reverseText();
+        System.out.print("After reverse: ");
+        doc.displayText();
+
+        
+        doc.deleteText(0, 5);
+        System.out.print("After delete: ");
+         doc.displayText();
+
+        
+        doc.clearText();
+        System.out.print("After clear: ");
+        doc.displayText();
+    }
+}
