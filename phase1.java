d14c3d1f72ac301123fa3db56646e1dace17f052
chhaya verma <vermachhaya93@gmail.com>
2026-06-05 09:36:00 +0530

    second class

--- phase1.java
+++ phase1.java
@@ -1,5 +1,5 @@
-class Document {
 
+class Document {
 
     private StringBuffer textBuffer;
 
@@ -7,32 +7,27 @@ class Document {
         textBuffer = new StringBuffer();
     }
 
-    
     public void addText(String input) {
         textBuffer.append(input);
     }
 
-    
     public void clearText() {
         textBuffer.setLength(0);
     }
-
 
     public void reverseText() {
         textBuffer.reverse();
     }
 
-    
     public void deleteText(int start, int end) {
-       
-    if (start >= 0 && end <= textBuffer.length() && start < end) {
-        textBuffer.delete(start, end);
-    } else {
-        System.out.println("Invalid index range");
-    }
-}    
 
-    
+        if (start >= 0 && end <= textBuffer.length() && start < end) {
+            textBuffer.delete(start, end);
+        } else {
+            System.out.println("Invalid index range");
+        }
+    }
+
     public void displayText() {
         System.out.println(textBuffer.toString());
     }
@@ -40,6 +35,7 @@ class Document {
 }
 
 public class phase1 {
+
     public static void main(String[] args) {
         Document doc = new Document();
         doc.addText("yahajdoa");
@@ -49,14 +45,13 @@ public class phase1 {
         System.out.print("After reverse: ");
         doc.displayText();
 
-        
         doc.deleteText(0, 5);
         System.out.print("After delete: ");
-         doc.displayText();
+        doc.displayText();
 
-        
         doc.clearText();
         System.out.print("After clear: ");
         doc.displayText();
     }
 }
+//second commit
